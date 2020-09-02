88b5a00f16f0469855f92e031ff5f4f7d246c535
Zellyk <jonathan.marcotte.7@gmail.com>
2020-09-02 15:03:54 -0400

    Add project files.

---
+++ StringBuilderDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("Hello");
+            sb.AppendLine("");
+            sb.AppendLine("This is the 2nd line");
+            sb.Append(".");
+
+            Console.WriteLine(sb.ToString());
+
+
+         
+
+        }
+    }
+}
